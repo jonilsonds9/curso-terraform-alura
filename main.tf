223cee830471033b2bb44447b32305cd44e97917
jonilsons9 <jonilsons9@gmail.com>
2020-01-15 16:57:56 -0300

    Aula 3

--- main.tf
+++ main.tf
@@ -11,7 +11,28 @@ resource "aws_instance" "dev" {
   tags = {
     Name = "dev${count.index}"
   }
-  vpc_security_group_ids = ["sg-0771b51d059604764"]
+  vpc_security_group_ids = ["${aws_security_group.acesso_ssh.id}"]
+}
+
+resource "aws_instance" "dev4" {
+  ami = "ami-04b9e92b5572fa0d1"
+  instance_type = "t2.micro"
+  key_name = "ubuntu-dell"
+  tags = {
+    Name = "dev4"
+  }
+  vpc_security_group_ids = ["${aws_security_group.acesso_ssh.id}"]
+  depends_on = [aws_s3_bucket.dev4]
+}
+
+resource "aws_instance" "dev5" {
+  ami = "ami-04b9e92b5572fa0d1"
+  instance_type = "t2.micro"
+  key_name = "ubuntu-dell"
+  tags = {
+    Name = "dev5"
+  }
+  vpc_security_group_ids = ["${aws_security_group.acesso_ssh.id}"]
 }
 
 resource "aws_security_group" "acesso_ssh" {
@@ -29,5 +50,14 @@ resource "aws_security_group" "acesso_ssh" {
 
   tags = {
     Name = "ssh"
+  }
+}
+
+resource "aws_s3_bucket" "dev4" {
+  bucket = "labs-dev4"
+  acl    = "private"
+
+  tags = {
+    Name        = "labs-dev4"
   }
 }
